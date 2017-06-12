2fcaffaeb035f5228e00172a13a73ff46bc17eff
MatheusXp30 <matheusr30@icloud.com>
2017-06-12 01:40:10 -0400

    Tela home e tela carrinho contando corretamente

--- script/carrinho.js
+++ script/carrinho.js
@@ -51,28 +51,33 @@ function imgLoader (id) {
 }
 
 function getCarrinho (tela) {
+    
     firebase.database().ref('/carrinho/' + userID + '/itens/').once('value').then(function(snapshot) {
         $("#carregando").hide();
 
         var itens = snapshot.val();
         if(tela == "home"){
-            $("#addCarrinho").text('(' + (itens.length - 1) + ') Carrinho');
+            $("#addCarrinho").text('(' + Object.keys(itens).length + ') Carrinho');
         }else if(tela == "carrinho"){
-            //       console.log(itens);
             firebase.database().ref('/bebidas/').once('value').then(function(snapshot2) {
-
-                var precoTotal = 0;
                 var itens2 = snapshot2.val();
-                //        console.log(itens2);
-                for(var count=1 ; count < itens.length ; count++){
+                var precoTotal = 0;
+                
+                var chavesCarrinho = Object.keys(itens);
+                var chavesBebidas = Object.keys(itens2);
+                
+                for(var count=0 ; count <= chavesCarrinho.length ; count++){
 
-                    for(var count2=1 ; count2 < itens2.length ; count2++){
+                    for(var count2=0 ; count2 <= chavesBebidas.length ; count2++){
 
-                        if(itens2[count2].id == itens[count].id){
-
-                            precoTotal += parseFloat(itens2[count2].preco);
-
-                            $("#preencherCarrinho").append('<tr class="productitm"><td id="itemCarrinho"><img src="img/bebidas/' + itens[count].id + '.png" class="thumb"></td><td id="qtdCarrinho"><input id="precoItem' + itens2[count2].id + '" type="number" value="' + itens[count].qtd + '" min="0" max="99" class="qtyinput"></td><td id="produtoCarrinho">' + itens2[count2].nome + '</td><td id="precoCarrinho">' + itens2[count2].preco + '</td><td><span class="remove"><img src="img/trash.png" alt="X"></span></td><td><button onclick="atualizarItem('+itens2[count2].id+')">Atualizar</button></td></tr>');
+                        if(chavesCarrinho[count] == chavesBebidas[count2]){
+                            
+                            var idProduto = chavesCarrinho[count];
+                            
+                            precoTotal += parseFloat(itens2[idProduto].preco);
+                            
+                            $("#preencherCarrinho").append('<tr class="productitm"><td id="itemCarrinho"><img src="img/bebidas/' + idProduto + '.png" class="thumb"></td><td id="qtdCarrinho"><input id="precoItem' + idProduto + '" type="number" value="' + itens[idProduto].qtd + '" min="0" max="99" class="qtyinput"></td><td id="produtoCarrinho">' + itens2[idProduto].nome + '</td><td id="precoCarrinho">' + itens2[idProduto].preco + '</td><td><span class="remove"><img src="img/trash.png" alt="X"></span></td><td><button onclick="atualizarItem('+idProduto+')">Atualizar</button></td></tr>');
+                            
                         }
                     }
                 }
